b1d19934dfb5fdbc8b74d5ae8703bf6bb3efc2cd
tanka78 <145720793+tanka78@users.noreply.github.com>
2024-04-07 22:38:26 -0600

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -35,6 +35,30 @@ DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
 )
 
+    def test_list_all_products(self):
+        """It should List all Products in the database"""
+        products = Product.all()
+        self.assertEqual(products, [])
+        # Create 5 Products
+        for _ in range(5):
+            product = ProductFactory()
+            product.create()
+        # See if we get back 5 products
+        products = Product.all()
+        self.assertEqual(len(products), 5)
+
+     def test_find_by_name(self):
+        """It should Find a Product by Name"""
+        products = ProductFactory.create_batch(5)
+        for product in products:
+            product.create()
+        name = products[0].name
+        count = len([product for product in products if product.name == name])
+        found = Product.find_by_name(name)
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.name, name)
+
 
 ######################################################################
 #  P R O D U C T   M O D E L   T E S T   C A S E S
