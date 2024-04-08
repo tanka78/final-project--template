b967f906e41c63b527a4234d7efc7666912a5e8c
tanka78 <145720793+tanka78@users.noreply.github.com>
2024-04-07 22:52:18 -0600

    Update load_steps.py

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -49,4 +49,10 @@ def step_impl(context):
     for row in context.table:
         #
         # ADD YOUR CODE HERE TO CREATE PRODUCTS VIA THE REST API
-        #
+            def test_get_product_list(self):
+        """It should Get a list of Products"""
+        self._create_products(5)
+        response = self.client.get(BASE_URL)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(len(data), 5)
